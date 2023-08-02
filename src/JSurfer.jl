efb21f2aeb204b332f3aa633b11c7c63affc59a3
Larry Eisenman <lneisenman@hotmail.com>
2023-08-02 14:44:04 -0500

    add readsurface function

--- src/JSurfer.jl
+++ src/JSurfer.jl
@@ -1,5 +1,55 @@
 module JSurfer
 
-# Write your package code here.
+export Surface, readsurface
+
+struct Surface
+    vertices::Array{Float32, 2}
+    faces::Array{Int32, 2}
+    stamp::String
+end
+
+
+function read3(io::IOStream)
+    test = read(io, 3)
+    return (Int(test[1]) << 16) + (Int(test[2]) << 8) + Int(test[3])
+end
+
+
+function readsurface(fn)
+    TRIANGLE::Int64 = 16777214
+    # QUAD::Int64 = 16777215
+    # NEW_QUAD::Int64 = 16777213
+
+    result::Int64 = 0
+    coords = Array{Float32, 2}[]
+    faces = Array{Int32, 2}[]
+    stamp::String = ""
+
+    open(fn, "r") do io
+        result = read3(io)
+        if result != TRIANGLE
+            ArgumentError("$fn is not a freesurfer file")
+        end
+        stamp = readline(io)
+        temp = readline(io)
+        vnum = ntoh(read(io, Int32))
+        fnum = ntoh(read(io, Int32))
+        coords = Array{Float32}(undef, vnum, 3)
+        faces = Array{Int32}(undef, fnum, 3)
+        for row in 1:vnum
+            for col in 1:3
+                coords[row, col] = ntoh(read(io, Float32))
+            end
+        end
+        for row in 1:fnum
+            for col in 1:3
+                faces[row, col] = ntoh(read(io, Int32))
+            end
+        end
+        faces .= faces .+ 1
+    end
+
+    return Surface(coords, faces, stamp)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using JSurfer
-using Test
+using Test, TestItems
 
-@testset "JSurfer.jl" begin
-    # Write your tests here.
+# @testset "JSurfer.jl" begin
+#     # Write your tests here.
+# end
+
+@testitem "test readsurface" begin
+    fn = "/mnt/Active/SEEG/subjects/LNE_2021/surf/lh.pial"
+    surf = readsurface(fn)
+    @test size(surf.vertices) == (147886, 3)
 end
